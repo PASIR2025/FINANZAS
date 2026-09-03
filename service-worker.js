1d4d0f16b5447c3eba5e2acebc4bf60dfad40b0b
PASIR2025 <pasir.industrial@gmail.com>
2026-09-03 00:02:29 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE='pasir-gestion-v4.6.1';
+const CORE=[
+  './','./index.html','./manifest.webmanifest','./assets/css/styles.css','./assets/js/app.js',
+  './assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/icons/icon-maskable-512.png',
+  './assets/icons/apple-touch-icon.png','./assets/icons/favicon-64.png','./assets/icons/pasir-logo.svg'
+];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin===location.origin){
+    event.respondWith(fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));
+  }else{
+    event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r})));
+  }
+});
